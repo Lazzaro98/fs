1d15bebbd84bb52092c2f95ab3625422aff11609
Lazzaro98 <46060203+Lazzaro98@users.noreply.github.com>
2022-01-05 01:15:25 +0100

    Test2
    Testing syntax in rust

--- test.rs
+++ test.rs
@@ -1,3 +1,13 @@
+fn get_char(string:String , i:usize) -> char {
+    return string.chars().nth(i).unwrap();
+}
+
+fn is_url_encoded(three_consecutive_chars:String){
+    // every url_encoded character's format is like %xy where xy is hexadecimal value
+    
+}
+
 fn main() {
-    println!("test");
+    let test:String = "Hello world".to_string();
+    is_url_encoded(test);
 }
